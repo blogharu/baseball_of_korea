bdeb42d9eb971c03e8e5fd9d3d1f1ca6d80bef36
blogharu <blogharu@gmail.com>
2023-07-19 21:48:55 -0500

    settings.py: DEBUG: apply .env

--- backend/django_backend/baseball_of_korea/settings.py
+++ backend/django_backend/baseball_of_korea/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True  # environ["DEBUG"] == "True"
+DEBUG = environ["DEBUG"] == "True"
 
 ALLOWED_HOSTS = ["baseball.blogharu.com", "192.168.*"] if not DEBUG else ["*"]
 
