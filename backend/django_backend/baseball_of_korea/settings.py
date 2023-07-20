03c4bb2b060d75a8ef0d2f75b69fa1df89bef6e1
blogharu <blogharu@gmail.com>
2023-07-19 21:49:35 -0500

    settings.py: remove STATICFILES_DIRS

--- backend/django_backend/baseball_of_korea/settings.py
+++ backend/django_backend/baseball_of_korea/settings.py
@@ -130,8 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "static"
-# STATICFILES_DIRS = [environ["STATICFILES_DIRS"]]
+STATIC_ROOT = environ["STATIC_ROOT"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
